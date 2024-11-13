e7c0c9e379a23c91d94485a81d6bcbaa5894bac3
SamueleTerraroli <terraroli99@gmail.com>
2024-11-13 14:17:40 +0100

    feat(prompt): add prompt in js

---
+++ script.js
@@ -0,0 +1,4 @@
+let età = parseInt(prompt ('indica la tua età'));
+console.log("Età:", età ,"Anni");
+let distanza = parseInt(prompt ('Quanti km devi percorrere?'));
+console.log("Distanza:", distanza,"Km");
